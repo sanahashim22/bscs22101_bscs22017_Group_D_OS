508e671951c541dd2f7a81af41de2f707e38900d
Haris Majeed <bscs22017@itu.edu.pk>
2024-08-30 23:43:15 +0500

    reading file

--- clie.c
+++ clie.c
@@ -7,7 +7,8 @@
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main() {
+int main()
+{
     /*
     WSADATA: A structure that contains information about the Windows Sockets implementation.
 SOCKET sock: A socket descriptor used to identify the socket.
@@ -17,14 +18,15 @@ message: The message to be sent to the server.
     WSADATA wsa;
     SOCKET sock;
     struct sockaddr_in server;
-    char *message = "Hello, Server!";
+    char *message = "C:\\Users\\HP\\OneDrive\\Desktop\\Semester 5\\Operating System\\Git Repo\\bscs22101_bscs22017_Group_D_OS\\cmds.txt";
     int server_addr_len = sizeof(server);
 
     // Initialize Winsock
-    /*WSAStartup: Initializes the Winsock library. The MAKEWORD(2,2) specifies the version of Winsock 
+    /*WSAStartup: Initializes the Winsock library. The MAKEWORD(2,2) specifies the version of Winsock
     to use (2.2 in this case).
     If initialization fails, the program prints an error and exits.*/
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
         printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
@@ -37,17 +39,18 @@ SOCK_STREAM for a stream socket (used for TCP).
 0 for the default protocol (TCP in this case).
 If socket creation fails, it prints an error, cleans up Winsock, and exits.
 */
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
         printf("Could not create socket : %d", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-/*server.sin_family: Specifies the address family (AF_INET for IPv4).
-server.sin_port: Specifies the port number, converted to network byte order using htons().
-server.sin_addr.s_addr: Specifies the server's IP address (127.0.0.1 for localhost) converted to 
-network byte order using inet_addr().
-*/
+    /*server.sin_family: Specifies the address family (AF_INET for IPv4).
+    server.sin_port: Specifies the port number, converted to network byte order using htons().
+    server.sin_addr.s_addr: Specifies the server's IP address (127.0.0.1 for localhost) converted to
+    network byte order using inet_addr().
+    */
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -57,7 +60,8 @@ network byte order using inet_addr().
     connect(): Attempts to establish a connection to the server using the provided server address and port.
     If the connection fails, the program prints an error, closes the socket, cleans up Winsock, and exits.
 */
-    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
         printf("Connect failed with error code : %d", WSAGetLastError());
         closesocket(sock);
         WSACleanup();
@@ -68,7 +72,8 @@ network byte order using inet_addr().
     /*send(): Sends the message to the server.
     If sending fails, it prints an error, closes the socket, cleans up Winsock, and exits.
     If the message is successfully sent, it prints "Message sent."*/
-    if (send(sock, message, strlen(message), 0) < 0) {
+    if (send(sock, message, strlen(message), 0) < 0)
+    {
         printf("Send failed with error code : %d", WSAGetLastError());
         closesocket(sock);
         WSACleanup();
@@ -78,8 +83,8 @@ network byte order using inet_addr().
     printf("Message sent\n");
 
     // Cleanup
-    //closesocket(): Closes the socket, releasing the resources associated with it.
-    //WSACleanup(): Cleans up the Winsock library.
+    // closesocket(): Closes the socket, releasing the resources associated with it.
+    // WSACleanup(): Cleans up the Winsock library.
     closesocket(sock);
     WSACleanup();
 
--- serv.c
+++ serv.c
@@ -3,84 +3,131 @@
 #include <string.h>
 #include <winsock2.h>
 
-
-//PORT: The port number on which the server is listening.
-//BUFFER_SIZE: The size of the buffer used to send/receive data.
+// PORT: The port number on which the server is listening.
+// BUFFER_SIZE: The size of the buffer used to send/receive data.
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-int main() {
+void process_file(const char *file_path)
+{
+    FILE *file = fopen(file_path, "r");
+    char line[BUFFER_SIZE];
+
+    if (file == NULL)
+    {
+        printf("Could not open file: %s\n", file_path);
+        return;
+    }
+
+    printf("File content:\n");
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        printf("%s", line);
+    }
+
+    fclose(file);
+}
+
+int main()
+{
     WSADATA wsa;
     SOCKET server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     char buffer[BUFFER_SIZE] = {0};
 
-    // Initialize Winsock
-     /*WSAStartup: Initializes the Winsock library. The MAKEWORD(2,2) specifies the version of Winsock 
-    to use (2.2 in this case).
-    If initialization fails, the program prints an error and exits.*/
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-        printf("Failed. Error Code : %d", WSAGetLastError());
+    /* Initialize Winsock
+     WSAStartup: Initializes the Winsock library. The MAKEWORD(2,2) specifies the version of Winsock
+       to use (2.2 in this case).
+       If initialization fails, the program prints an error and exits. */
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+    {
+        printf("Failed. Error Code : %d\n", WSAGetLastError());
         return 1;
     }
 
-    // Create socket   
-    //Sockets are used in networking to connect different computers or devices so they can communicate with each other.
-    //The socket() function creates a new socket. This is similar to setting up a communication channel that can be used to send or receive data. 
-    //syntax-> int var = socket(int domain , int type, int protocol)  domain is 
-    //internet protocol version means address family and type is which type of protocol we are using and 
-    //protocol we set 0 eacsue 00 is default tcp port
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Could not create socket : %d", WSAGetLastError());
+    /*Create socket
+    Sockets are used in networking to connect different computers or devices so they can communicate with each other.
+    The socket() function creates a new socket. This is similar to setting up a communication channel that can be used to send or receive data.
+    syntax-> int var = socket(int domain , int type, int protocol)  domain is
+    internet protocol version means address family and type is which type of protocol we are using and
+    protocol we set 0 because 0 is default tcp port*/
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d\n", WSAGetLastError());
         return 1;
     }
 
-    // Prepare the sockaddr_in structure
-    /*server.sin_family: Specifies the address family (AF_INET for IPv4).
-server.sin_port: Specifies the port number, converted to network byte order using htons().
-server.sin_addr.s_addr: Specifies the server's IP address (127.0.0.1 for localhost) converted to 
-network byte order using inet_addr().
-*/
+    /* Prepare the sockaddr_in structure
+    server.sin_family: Specifies the address family (AF_INET for IPv4).
+       server.sin_port: Specifies the port number, converted to network byte order using htons().
+       server.sin_addr.s_addr: Specifies the server's IP address (127.0.0.1 for localhost) converted to
+       network byte order using inet_addr(). */
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Bind
-    //The bind() function assigns the address specified by address to the socket.
-    //This is necessary so that the socket has a known address for clients to connect to.
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR) {
-        printf("Bind failed with error code : %d", WSAGetLastError());
+    /*Bind
+    The bind() function assigns the address specified by address to the socket.
+    This is necessary so that the socket has a known address for clients to connect to.*/
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code : %d\n", WSAGetLastError());
         closesocket(server_fd);
         WSACleanup();
         return 1;
     }
 
-    // Listen for incoming connections
-    //The listen() function marks the socket as a passive socket that will be used to accept incoming connection requests using accept().
+    /* Listen for incoming connections
+     The listen() function marks the socket as a passive socket that will be used to accept incoming connection requests using accept().*/
     listen(server_fd, 3);
 
-    // Accept incoming connection
-    //The accept() function is used to accept an incoming connection request from a client.
-    //It creates a new socket (new_socket) to handle the communication with the client while leaving 
-    //the original socket (server_fd) to continue listening for new connections.
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (int *)&addrlen)) == INVALID_SOCKET) {
-        printf("Accept failed with error code : %d", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        return 1;
+    printf("Server is running and waiting for connections on port %d...\n", PORT);
+
+    // Server loop to keep running and accepting new connections
+    while (1)
+    {
+        printf("Waiting for a new connection...\n");
+
+        /*Accept incoming connection
+        The accept() function is used to accept an incoming connection request from a client.
+        It creates a new socket (new_socket) to handle the communication with the client while leaving
+        the original socket (server_fd) to continue listening for new connections.*/
+        new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
+        if (new_socket == INVALID_SOCKET)
+        {
+            printf("Accept failed with error code : %d\n", WSAGetLastError());
+            closesocket(server_fd);
+            WSACleanup();
+            return 1;
+        }
+
+        /*Receive a message from client
+        The recv() function is used to receive data from the client.
+        It reads the data into the buffer.*/
+        int bytes_received = recv(new_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received > 0)
+        {
+            buffer[bytes_received] = '\0'; // Null-terminate the received data
+            printf("Filepath from client: %s\n", buffer);
+            process_file(buffer);
+        }
+        else if (bytes_received == 0)
+        {
+            printf("Client disconnected.\n");
+        }
+        else
+        {
+            printf("Receive failed with error code : %d\n", WSAGetLastError());
+        }
+
+        /*Cleanup for the current connection
+        After the communication is complete, closesocket() is used to close the sockets, and WSACleanup()
+        is called to clean up resources used by Winsock.*/
+        closesocket(new_socket);
     }
 
-    // Receive a message from client
-    //The recv() function is used to receive data from the client.
-    //It reads the data into the buffer.
-    recv(new_socket, buffer, BUFFER_SIZE, 0);
-    printf("Message from client: %s\n", buffer);
-
-    // Cleanup
-    //After the communication is complete, closesocket() is used to close the sockets, and WSACleanup()
-    // is called to clean up resources used by Winsock.
-    closesocket(new_socket);
+    // Cleanup after the server stops (though this part is unlikely to be reached)
     closesocket(server_fd);
     WSACleanup();
 
